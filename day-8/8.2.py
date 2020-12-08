a2c86a81a235ced4b25791685442292cb2ab72bc
Riccardo Leschiutta <leschiutta@office.mailup.it>
2020-12-08 12:59:46 +0100

    Refactoring

--- day-8/8.2.py
+++ day-8/8.2.py
@@ -9,22 +9,21 @@ def read_instructions():
     with open(file_path) as f:
         for line in (line.strip() for line in f):
             instructions.append((line[:3], line[4:]))
+            
     return instructions
 
 
-def solve_with_variant(instructions, variant_jmp_instruction=None, variant_nop_instruction=None):
+def solve_with_variant(instructions, variant_instruction=None):
     Solution = namedtuple(
         'Solution',
         ['accumulator',
-         'seen_jmp_instructions',
-         'seen_nop_instructions',
+         'seen_variants',
          'terminated']
     )
 
     accumulator = instruction_number = 0
     executed_instructions = set()
-    seen_jmp_instructions = []
-    seen_nop_instructions = []
+    seen_variants = []
     terminated = False
 
     while (instruction_number not in executed_instructions and
@@ -37,44 +36,35 @@ def solve_with_variant(instructions, variant_jmp_instruction=None, variant_nop_i
         executed_instructions.add(instruction_number)
         ins, num = instructions[instruction_number]
 
-        if instruction_number == variant_jmp_instruction:
-            ins = "nop"
-        elif instruction_number == variant_nop_instruction:
-            ins = "jmp"
+        if instruction_number == variant_instruction:
+            ins = "nop" if ins == "jmp" else "jmp"
 
         if ins == "nop":
-            seen_nop_instructions.append(instruction_number)
+            seen_variants.append(instruction_number)
             instruction_number += 1
         elif ins == "acc":
             accumulator += int(num)
             instruction_number += 1
         elif ins == "jmp":
-            seen_jmp_instructions.append(instruction_number)
+            seen_variants.append(instruction_number)
             instruction_number += int(num)
 
     return Solution(
         accumulator=accumulator,
-        seen_jmp_instructions=seen_jmp_instructions,
-        seen_nop_instructions=seen_nop_instructions,
+        seen_variants=seen_variants,
         terminated=terminated
     )
 
 
 def solve(instructions):
     solution = solve_with_variant(instructions)
-    seen_jmp_instructions = solution.seen_jmp_instructions
-    seen_nop_instructions = solution.seen_nop_instructions
-
+    variants = solution.seen_variants
     while not solution.terminated:
-        if len(seen_jmp_instructions) > 0:
-            jmp_variant = seen_jmp_instructions.pop()
+        if len(variants) > 0:
             solution = solve_with_variant(
-                instructions, variant_jmp_instruction=jmp_variant)
-        elif len(seen_nop_instructions) > 0:
-            nop_variant = seen_nop_instructions.pop()
-            solution = solve_with_variant(
-                instructions, variant_nop_instruction=nop_variant)
-
+                instructions, 
+                variant_instruction=variants.pop())
+            
     return solution.accumulator
 
 
