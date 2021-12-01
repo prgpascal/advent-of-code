e732e8b11af6a58ae2a56360f6cf2c7f4ad1c7d0
prgpascal <leschiutta.ricc@gmail.com>
2021-12-01 09:21:49 +0100

    Solved day 1

---
+++ 2021/day-01/1.1.py
@@ -0,0 +1,44 @@
+import os
+from collections import deque
+
+def read_input():
+    with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
+        input = [int(line.strip()) for line in file.readlines()]
+    return input
+
+
+def solve1(input):
+    previous = input[0]
+    counter = 0
+    for current in input:
+        if current > previous:
+            counter += 1
+        previous = current
+    return counter
+
+
+def solve2(input):
+    window = deque(maxlen=3)
+    previous_sum = sum(input[0:3])
+    counter = 0
+    for current in input:
+        window.append(current)
+        if len(window) == 3:
+            current_sum = sum(window)
+            if current_sum > previous_sum:
+                counter = counter +1
+            previous_sum = current_sum
+    return counter
+
+
+def write_output(output1, output2):
+    with open(os.path.join(os.path.dirname(__file__), "output.txt"), "w") as file:
+        file.write(f"{output1}\n")
+        file.write(f"{output2}")
+    print(output1, output2)
+
+
+input = read_input()
+output1 = solve1(input)
+output2 = solve2(input)
+write_output(output1, output2)
