4fb87b04a226b1832cd33bd8c0512b413fe36b70
prgpascal <leschiutta.ricc@gmail.com>
2021-12-02 20:03:32 +0100

    Added pyproject.toml. Refactoring.

--- 2021/day-01/solution.py
+++ 2021/day-01/solution.py
@@ -1,10 +1,12 @@
 import os
 from collections import deque
 
+
 def read_input():
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
         input = [int(line.strip()) for line in file.readlines()]
     return input
+
 
 def solve_1(input):
     previous = input[0]
@@ -15,6 +17,7 @@ def solve_1(input):
         previous = current
     return counter
 
+
 def solve_2(input):
     window = deque(maxlen=3)
     previous_sum = sum(input[0:3])
@@ -24,14 +27,16 @@ def solve_2(input):
         if len(window) == 3:
             current_sum = sum(window)
             if current_sum > previous_sum:
-                counter = counter +1
+                counter += 1
             previous_sum = current_sum
     return counter
+
 
 def write_output(output_1, output_2):
     with open(os.path.join(os.path.dirname(__file__), "output.txt"), "w") as file:
         file.write(f"{output_1}\n{output_2}")
     print(output_1, output_2)
+
 
 input = read_input()
 write_output(solve_1(input), solve_2(input))
--- 2021/day-02/solution.py
+++ 2021/day-02/solution.py
@@ -1,12 +1,14 @@
 import os
 
+
 def read_input():
     with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
-        input = []    
+        input = []
         for line in file:
             command, value = line.strip().split(" ")
             input.append((command, int(value)))
     return input
+
 
 def solve_1(input):
     x_axis_value = 0
@@ -20,6 +22,7 @@ def solve_1(input):
             y_axis_value -= value
     return x_axis_value * y_axis_value
 
+
 def solve_2(input):
     x_axis_value = 0
     y_axis_value = 0
@@ -27,17 +30,19 @@ def solve_2(input):
     for (command, value) in input:
         if command == "forward":
             x_axis_value += value
-            y_axis_value += (aim * value)
+            y_axis_value += aim * value
         elif command == "down":
             aim += value
         else:
             aim -= value
     return x_axis_value * y_axis_value
 
+
 def write_output(output_1, output_2):
     with open(os.path.join(os.path.dirname(__file__), "output.txt"), "w") as file:
         file.write(f"{output_1}\n{output_2}")
     print(output_1, output_2)
+
 
 input = read_input()
 write_output(solve_1(input), solve_2(input))
