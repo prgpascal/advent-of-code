09e77aa97ad58a1c93cb3e470958c53b972e25bd
prgpascal <leschiutta.ricc@gmail.com>
2021-12-11 10:31:49 +0100

    Added useful function to print multidimensional arrays

--- utils/io.py
+++ utils/io.py
@@ -6,3 +6,9 @@ def write_output(dirname, output_1, output_2):
     print(output)
     with open(os.path.join(dirname, "output.txt"), "w") as file:
         file.write(output)
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+    print()
